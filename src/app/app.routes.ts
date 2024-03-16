d334d939babe1e524702861fe30d7139b11e77ca
Pugijsons <hariis@inbox.lv>
2024-03-16 15:44:45 +0200

    logic to retrieve appointment data

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,6 +3,7 @@ import { AuthGuard } from './core/auth.guard';
 import { LoginComponent } from './login/login.component';
 import { RegisterComponent } from './register/register.component';
 import { UserComponent } from './user/user.component';
+import { CalendarComponent } from './calendar/calendar.component';
 import { securityInnerGuard } from './core/security-inner.guard';
 import {ChatComponent} from "./chat/chat.component";
 import {HomeComponent} from "./home/home.component";
@@ -10,6 +11,7 @@ import {HomeComponent} from "./home/home.component";
 export const routes: Routes = [
     { path: '', redirectTo: 'home', pathMatch: 'full' },
     { path: 'home', component:HomeComponent},
+    { path: 'calendar', component:CalendarComponent },
     { path: 'login', component: LoginComponent, canActivate: [AuthGuard] },
     { path: 'register', component: RegisterComponent, canActivate: [AuthGuard] },
     { path: 'user', component: UserComponent, canActivate: [securityInnerGuard] },
---
+++ src/app/calendar/calendar.component.ts
@@ -0,0 +1,40 @@
+import {Component, CUSTOM_ELEMENTS_SCHEMA, OnInit} from '@angular/core';
+import { AngularFireAuth } from '@angular/fire/compat/auth';
+import { AngularFirestore } from '@angular/fire/compat/firestore';
+import { Observable, switchMap } from 'rxjs';
+import { Router } from '@angular/router';
+import { CommonModule } from '@angular/common'; // Import CommonModule
+import {JsonPipe, NgForOf, NgIf} from "@angular/common";
+
+@Component({
+  selector: 'app-calendar',
+  standalone: true,
+  imports: [CommonModule, JsonPipe], 
+  templateUrl: './calendar.component.html',
+  schemas: [CUSTOM_ELEMENTS_SCHEMA]
+})
+
+export class CalendarComponent implements OnInit 
+{
+  currentDate : Date | undefined;
+  latestDates: any[] | undefined;
+  constructor(private firestore: AngularFirestore, private auth: AngularFireAuth, private router: Router) { }
+
+  ngOnInit(): void {
+    this.getLatestDates().subscribe(x => this.latestDates = x);
+  }
+
+  getLatestDates(): Observable<any[]> {
+    console.log("init");
+    return this.auth.authState.pipe(
+      switchMap(user => {
+        if (user) {
+          let collection = this.firestore.collection(`user/${user.uid}/appointments`);
+          return collection.valueChanges();
+        } else {
+          return []; // or throw an error/return an observable error
+        }
+      })
+    );
+  }
+}
