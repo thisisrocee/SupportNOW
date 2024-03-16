f814ba9db0a0ba43af08dd0ba09b3e5c2167f0e1
toms-gruzins <163577609+toms-gruzins@users.noreply.github.com>
2024-03-16 21:03:49 +0200

    calendar demo

--- src/app/calendar/calendar.component.ts
+++ src/app/calendar/calendar.component.ts
@@ -1,5 +1,5 @@
 import {Component, CUSTOM_ELEMENTS_SCHEMA, OnInit} from '@angular/core';
-import { CommonModule } from '@angular/common';
+import {CommonModule, NgForOf, NgIf} from '@angular/common';
 import {JsonPipe} from "@angular/common";
 import { AppointmentService } from './appointment-service';
 
@@ -7,18 +7,23 @@ import { AppointmentService } from './appointment-service';
   selector: 'app-calendar',
   standalone: true,
   providers: [AppointmentService],
-  imports: [CommonModule, JsonPipe], 
+  imports: [CommonModule, JsonPipe, NgForOf, NgIf],
   templateUrl: './calendar.component.html',
   schemas: [CUSTOM_ELEMENTS_SCHEMA]
 })
 
-export class CalendarComponent implements OnInit 
+export class CalendarComponent implements OnInit
 {
   currentDate : Date | undefined;
   latestDates: any[] | undefined;
+
   constructor(private appointmentService: AppointmentService) { }
 
   ngOnInit(): void {
     this.appointmentService.getLatestDates().subscribe((x: any[]) => this.latestDates = x);
   }
+
+  timeSlots = ["9:00", "10:00", "11:00", "12:00", "13:00", "14:00", "15:00", "16:00", "17:00"];
+  days = [ "2024-03-18", "2024-03-19", "2024-03-20", "2024-03-21", "2024-03-22"];
+
 }
