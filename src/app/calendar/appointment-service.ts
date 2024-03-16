f687911eb21445d78865cc0de1d059b70baff7ac
Pugijsons <hariis@inbox.lv>
2024-03-16 20:51:26 +0200

    ee

--- src/app/calendar/appointment-service.ts
+++ src/app/calendar/appointment-service.ts
@@ -1,6 +1,6 @@
 import { Injectable } from '@angular/core';
 import { AngularFireAuth } from '@angular/fire/compat/auth';
-import { AngularFirestore, QueryFn } from '@angular/fire/compat/firestore';
+import { AngularFirestore } from '@angular/fire/compat/firestore';
 import { Observable, switchMap } from 'rxjs';
 
 @Injectable()
