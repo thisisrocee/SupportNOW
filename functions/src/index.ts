b076154dd78da37d3c18e11096ee0ac6f31a2aa5
Rihard4a <118355184+Rihard4a@users.noreply.github.com>
2024-03-17 09:27:13 +0200

    functions fuck off

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -97,12 +97,10 @@ export const get_available_appointment_time = functions
     const { appointment_date } = req.query;
     console.log('appointment_date:', appointment_date);
 
-    // random 2 times from array
-    const availableTimes = ['08:00', '09:00','10:00', '11:00', '12:00', '13:00', '14:00', '15:00', '16:00', '17:00', '18:00', '19:00', '20:00'];
-    const randomTimes = availableTimes.sort(() => 0.5 - Math.random()).slice(0, 2);
+    const availableTimes = ['09:00', '10:00'];
 
-    await updateSession(sessionId, { availableTimes: randomTimes });
-    res.status(200).json({ results: randomTimes });
+    await updateSession(sessionId, { availableTimes: availableTimes });
+    res.status(200).json({ results: availableTimes });
   });
 
 export const select_appointment_time = functions
