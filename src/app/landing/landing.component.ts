c91dbf0f5022cdac60b7ff65e6e0dbd2d70d8633
Roberts Cernavskis <roberts.juniors@gmail.com>
2024-03-17 14:02:55 +0200

    bruh

--- src/app/landing/landing.component.ts
+++ src/app/landing/landing.component.ts
@@ -13,6 +13,13 @@ import { AppointmentService } from '../calendar/appointment-service';
 export class LandingComponent implements OnInit {
 
   ngOnInit(): void {
-
+    this.loadJsFile("assets/js/main.js");  
   }
+
+  public loadJsFile(url: string) {  
+    let node = document.createElement('script');  
+    node.src = url;  
+    node.type = 'text/javascript';  
+    document.getElementsByTagName('head')[0].appendChild(node);  
+  }  
 }
